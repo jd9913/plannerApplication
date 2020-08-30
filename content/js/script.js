044ff18491c8444cc766cb6b85ce594949882520
Jennifer Schlierman <jd9913@gmail.com>
2020-08-29 21:39:10 -0700

    added save to local storage

--- content/js/script.js
+++ content/js/script.js
@@ -1,94 +1,106 @@
-const workDay = [
+let workDay = [
     {
         id: "8",
         hour: "0800",
-        listItem: ""
+        taskItem: ""
     },
     {
         id: "9",
         hour: "0900",
-        listItem: ""
+        taskItem: ""
     },
     {
         id: "10",
         hour: "1000",
-        listItem: ""
+        taskItem: ""
     },
     {
         id: "11",
         hour: "1100",
-        listItem: ""
+        taskItem: ""
     },
     {
         id: "12",
         hour: "1200",
-        listItem: ""
+        taskItem: ""
     },
     {
         id: "13",
         hour: "1300",
-        listItem: ""
+        taskItem: ""
     },
     {
         id: "14",
         hour: "1400",
-        listItem: ""
+        taskItem: ""
     },
     {
         id: "15",
         hour: "1500",
-        listItem: ""
+        taskItem: ""
     },
     {
         id: "16",
         hour: "1600",
-        listItem: ""
+        taskItem: ""
     },
     {
         id: "17",
         hour: "1700",
-        listItem: ""
+        taskItem: ""
     }
-   
+
 ]
+
+
+//Tasks:
+//Ensure that textArea is being stored in the workDay Object as the value to key taskItem;
+//create the event for the save button to trigger the save function to local storage.
+//test to ensure that it works.
+
 
 const containerEl = document.getElementById('#container');
 const initRow = document.getElementsByClassName("row");
+let taskList = JSON.parse(localStorage.getItem("my_workDay")) || [];
+let taskArea = workDay.taskItem;
 
 $(document).ready(function () {
 
     console.log("ready!");
 
+  
 
-//get the current date and place it in the header
+    //get the current date and place it in the header
     function getCurrentDay() {
         const currentDay = moment().format('dddd, MMMM Do, YYYY');
         $('#currentDay').text(currentDay);
     }
     console.log(currentDay);
 
-    //set any existing data to the view if it exists in Local Storage
 
+    //place the current day/date in the header field
 
-    //save data to local storage
+    getCurrentDay();
+
 
     //create the visual, dynamic grid for the daily planner
 
 
     workDay.forEach(function (eachHour) {
-            
+
         //These create the row and place the time and save buttons in the appropriate places for each row.
         const begInput = $("<div>").attr({ "class": "input-group" });
         $(initRow).append(begInput);
-        
+
         const eachRow = $("<div>").attr({ "class": "input-group-prepend col-sm" });
         $(begInput).append(eachRow);
 
         const eachTime = $("<span>").attr({ "class": "input-group-text justify-content-left" }).text(eachHour.hour);
         $(eachRow).append(eachTime);
 
-        const textArea = $('<textarea>').attr({ "class": "form-control" });
+        textArea = $('<textarea>').attr({ "class": "form-control" });
         $(eachTime).append(textArea);
+        
 
         const saveBtn = $('<div>').attr({ "class": "input-group-append" });
         $(eachTime).append(saveBtn);
@@ -112,34 +124,59 @@ $(document).ready(function () {
             })
         } else if (eachHour.hour > moment().format("HHHH")) {
             textArea.attr({
-                "class":"form-control future"
+                "class": "form-control future"
             })
         }
-        
     
+
+
+
+
+
+        workDay.taskItem = $(textArea).val().trim();
+
+        taskList.push(workDay);
+
+        localStorage.setItem('my_workDay', JSON.stringify(taskList));
+
+
+        $('.saveBtn').on('click', function (event) {
+            event.preventDefault();
+            let saveIndex = $(this).siblings(".taskDescription").children(".future").attr('id');
+            workDay[saveIndex].taskItem = $(this).siblings(".taskDescription").children("future").val();
+            console.log(saveIndex);
+        });
+
+       
+
         
+
+
+
+
     });
-
-    
-
-    
-
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-//place the current day/date in the header field
-
-    getCurrentDay();
-
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
